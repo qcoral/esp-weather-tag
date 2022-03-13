f3ed951d5ee9b8bb105113644f2cf0b17cf8b0cc
Dari <41168529+dari-studios@users.noreply.github.com>
2022-03-13 00:38:44 -0500

    all basic features implemented, bugfixes + graphics next.

--- esp-weather-tag.ino
+++ esp-weather-tag.ino
@@ -1,85 +1,129 @@
+#include <LOLIN_EPD.h>
+#include <Adafruit_GFX.h>
 #include <time.h>
 #include <ArduinoJson.h>
 #include <WiFiManager.h>
+#include <PolledTimeout.h>
 #include <ESP8266HTTPClient.h>
 
 #define MY_NTP_SERVER "ca.pool.ntp.org"
 #define MY_TZ "EST5EDT,M3.2.0,M11.1.0"
 
-time_t now;
-tm tm;    
+#define EPD_CS D8    // GPIO 15
+#define EPD_DC D4    // GPIO 2
+#define EPD_RST D1   // GPIO 5
+#define EPD_BUSY -1 // Waits a fixed delay, saves pins
 
-void showTime() {
-  time(&now);                       // read the current time
-  localtime_r(&now, &tm);           // update the structure tm with the current time
-  Serial.print("year:");
-  Serial.print(tm.tm_year + 1900);  // years since 1900
-  Serial.print("\tmonth:");
-  Serial.print(tm.tm_mon + 1);      // January = 0 (!)
-  Serial.print("\tday:");
-  Serial.print(tm.tm_mday);         // day of month
-  Serial.print("\thour:");
-  Serial.print(tm.tm_hour);         // hours since midnight  0-23
-  Serial.print("\tsec:");
-  Serial.print(tm.tm_sec);          // seconds after the minute  0-61*
-  Serial.print("\twday");
-  Serial.print(tm.tm_wday);         // days since Sunday 0-6
-  Serial.println();
+LOLIN_SSD1680 EPD(250, 122, EPD_DC, EPD_RST, EPD_CS, EPD_BUSY); //hardware SPI
+
+time_t now;
+tm tm;
+
+void printMillis() {
+  Serial.print(F("millis() = "));
+  Serial.println(millis());
+} 
+
+void wakeupCallback() {  // unlike ISRs, you can do a print() from a callback function
+  printMillis();  // show time difference across sleep; millis is wrong as the CPU eventually stops
+  Serial.println(F("Woke from Light Sleep - this is the callback"));
+  Serial.flush();
+}
+
+void printTime() {
+   time(&now);                       // read the current time
+   localtime_r(&now, &tm);           // update the structure tm with the current time
+   Serial.print("year:");
+   Serial.print(tm.tm_year + 1900);  // years since 1900
+   Serial.print("\tmonth:");
+   Serial.print(tm.tm_mon + 1);      // January = 0 (!)
+   Serial.print("\tday:");
+   Serial.print(tm.tm_mday);         // day of month
+   Serial.print("\thour:");
+   Serial.print(tm.tm_hour);         // hours since midnight  0-23
+   Serial.print("\tsec:");
+   Serial.print(tm.tm_sec);          // seconds after the minute  0-61*
+   Serial.print("\twday");
+   Serial.print(tm.tm_wday);         // days since Sunday 0-6
+   Serial.println();
+   EPD.clearBuffer();
+   EPD.setCursor(0, 0);
+   EPD.setTextColor(EPD_RED);
+   EPD.setTextWrap(true);
+   EPD.print(String(tm.tm_mday));
+   Serial.println("displaying date");
+   EPD.display();
+   delay(25000);
 }
 
 void setup() {
-	WiFi.mode(WIFI_STA);
-  	// Initialize serial port
-	Serial.begin(9600);
-
-  configTime(MY_TZ, MY_NTP_SERVER);
-
-	WiFiManager wm;
-
-	bool connectresult;
-	connectresult = wm.autoConnect();
-	if(!connectresult) {
-		Serial.println("Failed to connect");
-		ESP.restart();
-	} 
-	else {
-		Serial.println("Connected");
-	}
-  time(&now);
-  localtime_r(&now, &tm);
-  delay(2000);     
+   // Initialize serial port
+   Serial.begin(9600);
+   EPD.begin();
+   configTime(MY_TZ, MY_NTP_SERVER);
 }
 
 void loop() {
-  do {
-    showTime();
-    delay(2000);
-  } while (tm.tm_hour > 0 || tm.tm_sec > 5);
-  
-	WiFiClient wc;
-	HTTPClient http;
-  
-	http.useHTTP10(true);
-	http.begin(wc, "http://api.openweathermap.org/data/2.5/onecall?lat=***REMOVED***&lon=***REMOVED***&exclude=minutely,daily,alerts,current&appid=***REMOVED***&units=metric&lang=en");  //Specify request destination
-	http.GET();                                  //Send the request
- 
-	DynamicJsonDocument doc(24576);
+   WiFi.mode(WIFI_STA);
+   WiFiManager wm;
+   
+   bool connectresult;
+   connectresult = wm.autoConnect();
+   if (!connectresult) {
+      Serial.println("Failed to connect");
+      ESP.restart();
+   }
+   else {
+      Serial.println("Connected");
+   }
 
-	DeserializationError error = deserializeJson(doc, http.getStream());
-  
-  http.end();   //Close connection
-  
-	if (error) {
-	  Serial.print(F("deserializeJson() failed: "));
-	  Serial.println(error.f_str());
-	  return;
-  }
-  for (int i = 0; i < 23; i++) {
-    Serial.print(F("temp hour "));
-    Serial.print(i);
-    Serial.print(F(": "));
-    Serial.print(String(doc["hourly"][i]["temp"]));
-    Serial.println(F("C"));
-    ESP.deepSleep(3595e6);
-  }
+   time(&now);
+   localtime_r(&now, &tm);
+   delay(2000);
+   
+   printTime();
+   WiFiClient wc;
+   HTTPClient http;
+
+   http.useHTTP10(true);
+   http.begin(wc, "http://api.openweathermap.org/data/2.5/onecall?lat=***REMOVED***&lon=***REMOVED***&exclude=minutely,daily,alerts,current&appid=***REMOVED***&units=metric&lang=en");  //Specify request destination
+   http.GET();                                  //Send the request
+
+   DynamicJsonDocument doc(24576);
+
+   DeserializationError error = deserializeJson(doc, http.getStream());
+
+   http.end();   //Close connection
+
+   if (error) {
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+      return;
+   }
+   for (int i = 0; i < 3; i++) {
+      Serial.print(F("temp hour "));
+      Serial.print(i);
+      Serial.print(F(": "));
+      Serial.print(String(doc["hourly"][i]["temp"]));
+      Serial.println(F("C"));
+      EPD.clearBuffer();
+      EPD.setCursor(0, 0);
+      EPD.setTextColor(EPD_BLACK);
+      EPD.setTextWrap(true);
+      EPD.setTextSize(5);
+      EPD.print(String(doc["hourly"][i]["temp"]));
+      EPD.display();
+      lightsleep();
+   }
+}
+
+void lightsleep() {
+  WiFi.mode(WIFI_OFF);  // you must turn the modem off; using disconnect won't work
+  extern os_timer_t *timer_list;
+  timer_list = nullptr;  // stop (but don't disable) the 4 OS timers
+  wifi_fpm_set_sleep_type(LIGHT_SLEEP_T);
+  wifi_fpm_set_wakeup_cb(wakeupCallback);
+  wifi_fpm_open();
+  wifi_fpm_do_sleep(10E6);
+  delay(25e3 + 1);
 }
