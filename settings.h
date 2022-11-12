21bf24f59a9fadfbdc7e97317b538d7804be9565
Dari <dari@daristudios.ca>
2022-11-12 18:36:54 -0500

    added settings file

---
+++ settings.h
@@ -0,0 +1,26 @@
+/*
+  ESP Weather Tag settings file. Please refer to README.md for more information. 
+  Copyright (C) 2022  dari-studios
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+#include <TZ.h>
+
+#define MY_NTP_SERVER "ca.pool.ntp.org"  //NTP server
+#define MY_TZ TZ_America_Toronto   //timezone set timezone
+
+#define LAT
+#define LON
+
+#define MY_API_KEY
